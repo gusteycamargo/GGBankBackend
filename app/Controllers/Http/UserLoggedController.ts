45fb7d89b5f4d643cc291b2835e10478693c51be
gustaacamargo <camargo25.gustavo@gmail.com>
2020-08-16 19:16:18 -0300

    Fix method index at userlogged controller

--- app/Controllers/Http/UserLoggedController.ts
+++ app/Controllers/Http/UserLoggedController.ts
@@ -1,9 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import User from 'App/Models/User'
 export default class UserLoggedController {
   public async index ({ auth }: HttpContextContract) {
     const user = await auth.authenticate()
+    const userLoggedWithAccount = await User.findOrFail(user.id)
+    await userLoggedWithAccount.preload('account')
 
-    return user
+    return userLoggedWithAccount
   }
 }
